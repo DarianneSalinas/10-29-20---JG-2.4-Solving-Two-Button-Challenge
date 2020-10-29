17aeb63847699c2cfa4c377ba4746ee7e7c61170
DarianneSalinas <darianne.salinas@gmail.com>
2020-10-29 04:15:51 -0600

    Initial Commit

---
+++ 10-29-20 - JG 2.4 Solving Two Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-29-20 - JG 2.4 Solving Two Button Challenge
+//
+//  Created by Darianne Salinas on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
